cd4aa3a750c0c0919f99ec6e371449ad02ecf56b
Dorian Barsoni <dbarsoni@gmail.com>
2024-02-21 14:20:49 +0100

    Creating UI to spawn knights and archers

---
+++ Source/MedievalDefense/PlayerUI.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerUI.h"
+#include "NavigationSystem.h"
+
+void UPlayerUI::onKnightButtonClicked(FVector SpawnPoint) {
+    if (KnightActor) {         
+        FActorSpawnParameters SpawnParams;
+        SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
+
+        AActor* NewTroopCharacter = GetWorld()->SpawnActor<AActor>(KnightActor, getSpawnPoint(SpawnPoint), FRotator::ZeroRotator, SpawnParams);
+    } else {
+        UE_LOG(LogTemp, Error, TEXT("TroopCharacterBlueprint not set in UPlayerUI"));
+    }
+}
+
+void UPlayerUI::onArcherButtonClicked(FVector SpawnPoint) {
+    if (ArcherActor) {
+        FActorSpawnParameters SpawnParams;
+        SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
+
+        AActor* NewTroopCharacter = GetWorld()->SpawnActor<AActor>(ArcherActor, getSpawnPoint(SpawnPoint), FRotator::ZeroRotator, SpawnParams);
+    }
+    else {
+        UE_LOG(LogTemp, Error, TEXT("TroopCharacterBlueprint not set in UPlayerUI"));
+    }
+}
+
+FVector UPlayerUI::getSpawnPoint(FVector SpawnPoint) {
+    UNavigationSystemBase* NavigationSystem = GetWorld()->GetNavigationSystem();
+    if (NavigationSystem) {
+        UNavigationSystemV1* NavigationSystemV1 = Cast<UNavigationSystemV1>(NavigationSystem);
+        if (NavigationSystemV1) {
+            FNavLocation RandomLocation;
+            if (NavigationSystemV1->GetRandomReachablePointInRadius(SpawnPoint, SpawnRadius, RandomLocation)) {
+                return RandomLocation.Location + FVector(0, 0, 90);
+            } else { UE_LOG(LogTemp, Error, TEXT("GetReachablePoint")); }
+        } else { UE_LOG(LogTemp, Error, TEXT("NavSystemV1")); }
+    }
+    else {
+        UE_LOG(LogTemp, Error, TEXT("NavSystem"));
+    }
+
+    return FVector(0, 0, 0);
+}
---
+++ Source/MedievalDefense/PlayerUI.h
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/Button.h"
+#include "Components/TextBlock.h"
+#include "PlayerUI.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable)
+class MEDIEVALDEFENSE_API UPlayerUI : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UButton* KnightButton;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UTextBlock* KnightText;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UButton* ArcherButton;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UTextBlock* ArcherText;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "TroopCharacter")
+	TSubclassOf<AActor> KnightActor;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "TroopCharacter")
+	TSubclassOf<AActor> ArcherActor;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float SpawnRadius;
+
+private:
+	UFUNCTION(BlueprintCallable)
+	void onKnightButtonClicked(FVector SpawnPoint);
+
+	UFUNCTION(BlueprintCallable)
+	void onArcherButtonClicked(FVector SpawnPoint);
+
+	FVector getSpawnPoint(FVector SpawnPoint);
+	
+};
